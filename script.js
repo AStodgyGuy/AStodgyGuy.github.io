bee1892aa492be6536ebfa853504c517d272103f
AStodgyGuy <justinyang28@googlemail.com>
2018-01-24 01:13:08 +0000

    first blog post plus other minor changes

--- script.js
+++ script.js
@@ -20,6 +20,10 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
+    $('#blog').click(function(e) {
+        window.location.href = "blog.html";
+    });
+
     $('[data-popup-close]').on('click', function(e)  {
         $('[data-popup="popup-1"]').fadeOut(350);
         e.preventDefault();
